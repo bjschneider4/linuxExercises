8eed52c6625a51ada9e1e3f37fa247a234dae658
Safa <seltgani@wisc.edu>
2024-02-28 18:47:34 -0600

    ...

---
+++ school.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d',' -f7 | {
+    sum=0
+    count=0
+    while read n; do
+        sum=$(($sum + $n))
+        count=$(($count + 1))
+    done
+    
+    if [ $count -gt 0 ]; then
+        echo "Total Sum: $sum"
+        average=$(echo "scale=2; $sum / $count" | bc)
+        echo "Average Total Assessed Value for MADISON SCHOOLS: $average"
+    else
+        echo "No records found for MADISON SCHOOLS."
+    fi
+}
