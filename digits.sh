04d15d7be17dc82e86943840eea89f7746cbb13e
Breanna Schneider <bjschneider4@wisc.edu>
2024-03-05 10:51:07 -0600

    main

--- digits.sh
+++ digits.sh
@@ -10,19 +10,6 @@ for num in {1000..2000}; do
     fi
     echo $sum;  done
 
-for num in {1000..2000}; do
-    number=$num
-    mytest=${BASH_REMATCH[1]}
-    if [[ $number =~ "(.)(.)(.)(.)" ]]; then
-	one=${BASH_REMATCH[1]}
-	two=${BASH_REMATCH[2]}
-	three=${BASH_REMATCH[3]}
-	four=${BASH_REMATCH[4]}
-	echo $one
-    else
-	continue
-    fi; done
-
 
 
 
