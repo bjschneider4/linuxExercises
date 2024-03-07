6d9743218fb9ea0f403b19e7eb03ddac9f629ba5
Tapasya Nangpal <nangpal@wisc.edu>
2024-03-07 11:46:42 -0600

    final changes to five_dirs

--- five_dirs.sh
+++ five_dirs.sh
@@ -7,7 +7,7 @@ for i in {1..5}; do
     # echo $i >> "$dir/file$i"
     for ((j=1; j <= 4; j++))
     do
-	file = "$dir/file$j"
+	file="$dir/file$j"
 	for ((k=1; k<=$j;k++))
 	do
 	    echo $j >> "$file"
