24364d725be52c24d0034480d798bc1175a3a90b
Tapasya Nangpal <nangpal@wisc.edu>
2024-03-07 02:19:04 -0600

    file and directory generation for five_dirs

---
+++ five_dirs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mkdir five
+
+for i in {1..5}; do
+    dir="five/dir$i"
+    mkdir -p "$dir"
+    # echo $i >> "$dir/file$i"
+    for ((j=1; j <= 4; j++))
+    do
+	file = "$dir/file$j"
+	for ((k=1; k<=$j;k++))
+	do
+	    echo $j >> "$file"
+	done
+    done
+done
